b90a280d13dcb7c1aae5c6ded80fb6b528fa5f13
Yogesh Kumar <117355987+1Yogeshh@users.noreply.github.com>
2024-12-23 17:06:20 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: ['https://auth-verify.vercel.app/', 'http://localhost:3000'],
+    origin: ['https://auth-verify.vercel.app', 'http://localhost:3000'],
     credentials: true,
   })
 );
